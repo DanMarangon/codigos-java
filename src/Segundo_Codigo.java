d881b5fa502926fe40eee619cf3ac3a52a9dab46
Dan <danramosmarques123@gmail.com>
2026-09-21 16:28:08 -0300

    Adicionando segundo código

--- src/Segundo_Codigo.java
+++ src/Segundo_Codigo.java
@@ -1,7 +1,14 @@
+import java.util.Scanner;
+
 public class Segundo_Codigo {
 
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Olá, informe o seu nome: ");
+        String nome=scanner.next();
+        System.out.println("Olá, informe a sua idade: ");
+        int idade=scanner.nextInt();
+        System.out.println("Olá, " + nome + " Sua idade é: " + idade);
 
 
     }
