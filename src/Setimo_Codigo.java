463dddc9b13f93e7c287546bb30a731a0ea1094c
Dan <danramosmarques123@gmail.com>
2026-09-22 18:15:00 -0300

    subindo código de soma simples em java

---
+++ src/Setimo_Codigo.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Setimo_Codigo {
+
+    public static void main(String[] Args){
+
+        var scanner = new Scanner(System.in);
+        System.out.println("Digite aqui o seu primeiro numero: ");
+        int numero1 = scanner.nextInt();
+        System.out.println("Digite aqui o seu segundo numero: ");
+        int numero2 = scanner.nextInt();
+
+        System.out.println("O resultado da sua soma é: "  + (numero1 + numero2));
+
+
+    }
+
+
+}
