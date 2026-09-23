bd2183b18c113201e859f387a5b86967c230eef8
Dan <danramosmarques123@gmail.com>
2026-09-23 17:49:04 -0300

    codigo em java utilizando while

---
+++ src/Codigo_12.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Codigo_12 {
+
+    public static void main(String[] Args){
+
+        String nome;
+        var scanner = new Scanner(System.in);
+
+        while(true){
+
+            System.out.println("Digite um nome: ");
+            nome = scanner.next();
+            System.out.println("Seja bem vindo (a): " + nome);
+
+            if(nome.equalsIgnoreCase("exit")){
+
+                break;
+
+            }
+
+
+        }
+
+    }
+
+
+}
